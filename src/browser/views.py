2c5f17971332471087b70a68351beedd376cc451
zjsxzy <zjsxzy@gmail.com>
2015-10-29 21:44:25 +0800

    update

--- src/browser/views.py
+++ src/browser/views.py
@@ -234,7 +234,8 @@ def repo_results(request, repo_base, repo):
         'login': get_login(request),
         'repo_base': repo_base,
         'repo': repo,
-        'base_tables': zip(base_tables, commit_ids),
+        'base_tables': base_tables,
+        'commit_ids': commit_ids,
         'views': views}
 
     res.update(csrf(request))
